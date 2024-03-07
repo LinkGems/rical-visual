53842ced8cc8976fc0c4fc1986f30483f6a8d45b
meidanlong <meidanlong@maoyan.com>
2024-03-07 22:09:10 +0800

    init

---
+++ src/main/java/org/linkgems/rical/visual/Main.java
@@ -0,0 +1,12 @@
+package org.linkgems.rical.visual;
+
+/**
+ * @description:
+ * @author: meidanlong
+ * @date: 2024/3/7 10:04 PM
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
